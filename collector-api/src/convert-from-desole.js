92a62e24d7764d46eb1cda0889a27e881b2dbc3c
dmaser <dave@moneyminder.com>
2019-05-31 14:37:46 -0700

    Add message to desole event

--- collector-api/src/convert-from-desole.js
+++ collector-api/src/convert-from-desole.js
@@ -6,7 +6,7 @@ const createGenericEventFromLambda = require('./create-generic-lambda-event'),
 module.exports = function convertFromDesole(lambdaProxyEvent, lambdaContext) {
 	const body = JSON.parse(lambdaProxyEvent.body),
 		genericEvent = createGenericEventFromLambda(lambdaProxyEvent),
-		desoleEvent = Object.assign(genericEvent, extractKeys(body, ['severity', 'stack', 'type', 'timestamp', 'resource', 'tags']));
+		desoleEvent = Object.assign(genericEvent, extractKeys(body, ['severity', 'stack', 'type', 'message', 'timestamp', 'resource', 'tags']));
 
 	Object.assign(desoleEvent.endpoint, extractKeys(body.endpoint, ['id', 'platform', 'language']));
 	desoleEvent.app = extractKeys(body.app, ['name', 'version', 'stage']);
